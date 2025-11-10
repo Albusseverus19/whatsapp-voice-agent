b537fb71865bed8e505794d2b358af041890286e
Albusseverus <ikarokva@gmail.com>
2025-11-11 00:49:59 +0400

    v.3

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+# ElevenLabs
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY", "")
+ELEVENLABS_VOICE_ID = os.getenv("ELEVENLABS_VOICE_ID", "")
+ELEVENLABS_MODEL_ID = os.getenv("ELEVENLABS_MODEL_ID", "eleven_v3")
+
+# Gemini
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+
+# Twilio
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER", "")
+
+# Media Stream WebSocket URL
+MEDIA_STREAM_WS_URL = os.getenv("MEDIA_STREAM_WS_URL", "")
